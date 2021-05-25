6d17bc0005262fd8883f6dba2d900f6e0e97535a
HTCho1 <sksmsghdxo@naver.com>
2021-05-25 18:09:14 +0900

    Heapq

--- heapq_.py
+++ heapq_.py
@@ -3,11 +3,9 @@ import heapq
 def heapsort(iterable):
     h = []
     result = []
-
     # 모든 원소를 차례대로 힙에 삽입
     for value in iterable:
         heapq.heappush(h, value)
-
     # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
     for i in range(len(h)):
         result.append(heapq.heappop(h))
@@ -17,15 +15,15 @@ def heapsort(iterable):
 result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
 print(result)
 
-#-------------------------------------------------#
+#-----------------------------------------------------#
 
 def heapsort_reverse(iterable):
     h = []
     result = []
-
+    # 모든 원소를 차례대로 힙에 삽입
     for value in iterable:
         heapq.heappush(h, -value)
-
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
     for i in range(len(h)):
         result.append(-heapq.heappop(h))
 
