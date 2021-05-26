61e7593b0abd634dc316973695b7caf639d3ee73
HTCho1 <sksmsghdxo@naver.com>
2021-05-26 16:09:39 +0900

    card game using min()

---
+++ 3. 그리디/숫자 카드 게임 min().py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+
+result = 0
+# 한 줄씩 입력받아 확인
+for i in range(n):
+    card = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_ = min(card)
+    # '가장 작은 수' 중에서 가장 큰 수 찾기
+    result = max(result, min_)
+
+print(result)
+
+#--------------------------------------------------#
+
+result = 0
+
+for i in range(n):
+    card = list(map(int, input().split()))
+
+    min_ = min(card)
+
+    if result < min_:
+        result = min_
+
+
+print(result)
