ceb88d36945cfd070e953f308307cd5b227814d7
HTCho1 <sksmsghdxo@naver.com>
2021-06-02 21:17:56 +0900

    add directory

--- [토크ON 세미나] 기초 알고리즘과 파이썬 코딩/string_to_int.py
+++ [토크ON 세미나] 기초 알고리즘과 파이썬 코딩/string_to_int.py
@@ -6,5 +6,13 @@ def stoi(s, n):
     for i in range(l): ret += int(s[i]) * n ** (l - i -1)
     return ret
 
-result = stoi('10505', 10)
-print(result)
+print(stoi('10505', 2))
+
+# 위 방식은 연산량이 많다.
+
+def stoi(s, n):
+    ret = 0
+    for i in s: ret = ret * n + int(i)
+    return ret
+
+print(stoi('10505', 2))
