2274eca842aae71d13a0f12151e30b6e37e3b986
HTCho1 <sksmsghdxo@naver.com>
2021-06-02 22:25:51 +0900

    소수판별법

---
+++ [토크ON 세미나] 기초 알고리즘과 파이썬 코딩/소수판별법.py
@@ -0,0 +1,20 @@
+# 2부터 N - 1까지 체크
+# 시간 복잡도가 O(N)
+def isPrime(N):
+    for i in range(2, N):
+        if N % i == 0: return print('소수가 아닙니다.')
+    return print('소수입니다.')
+
+isPrime(19)
+
+
+# 2부터 sqrt(N)까지 체크
+# 시간 복잡도가 O(sqrt(N))
+def isPrime(N):
+    i = 2
+    while i * i <= N:
+        if N % i == 0: return print('소수가 아닙니다.')
+        i += 1
+    return print('소수입니다.')
+
+isPrime(19)
