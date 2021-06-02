9231f290e019d2a47f7df6b3303957b0a1ecdbe1
HTCho1 <sksmsghdxo@naver.com>
2021-06-03 04:35:54 +0900

    add a function

--- [토크ON 세미나] 기초 알고리즘과 파이썬 코딩/3강 - 알고리즘보다 코딩2 - 수학/gcd.py
+++ [토크ON 세미나] 기초 알고리즘과 파이썬 코딩/3강 - 알고리즘보다 코딩2 - 수학/gcd.py
@@ -5,6 +5,16 @@ def gcd(a, b):
 
 print(gcd(33, 60))
 
+def gcd(a, b):
+    ret = 0
+    for i in range(min(a, b)):
+        if i == 0: continue
+        if a % i == 0 and b % i == 0:
+            ret = i
+    return ret
+
+print(gcd(33, 60))
+
 # 유클리드 호제법
 # GCD(a, b) = GCD(b, a % b)
 
