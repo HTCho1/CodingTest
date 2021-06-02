e00befa77ab4b5ac793ed0de2c40e4c6dd7eac36
HTCho1 <sksmsghdxo@naver.com>
2021-06-03 05:05:11 +0900

    add remark

--- [토크ON 세미나] 기초 알고리즘과 파이썬 코딩/3강 - 알고리즘보다 코딩2 - 수학/소수 찾기(BOJ).py
+++ [토크ON 세미나] 기초 알고리즘과 파이썬 코딩/3강 - 알고리즘보다 코딩2 - 수학/소수 찾기(BOJ).py
@@ -4,7 +4,10 @@ def isPrime(N):
     N = int(N)
     if N == 1: return False
     for i in range(2, N):
+        # N이 i로 나누어 떨어지면 소수가 아니므로 False 반환
         if N % i == 0: return False
+    # i를 N - 1까지 반복하는 동안,
+    # N을 나눈 나머지가 0이 되는 i가 없으면 소수이므로 True 반환
     return True 
 
 '''
@@ -15,8 +18,11 @@ def isPrime(N):
     i = 2
     if N == 1: return False
     while i * i <= N:
+        # N이 i로 나누어 떨어지면 소수가 아니므로 False 반환
         if N % i == 0: return False
         i += 1
+    # i * i가 N까지 반복될 동안,
+    # N을 나눈 나머지가 0이 되는 i가 없으면 소수이므로 True 반환
     return True
 
 input()
