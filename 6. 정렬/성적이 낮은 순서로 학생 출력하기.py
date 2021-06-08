c9d0d1f901baecb9a924c90476e254d8a95d1be9
HTCho1 <sksmsghdxo@naver.com>
2021-06-08 18:18:55 +0900

    revise method

--- 6. 정렬/성적이 낮은 순서로 학생 출력하기.py
+++ 6. 정렬/성적이 낮은 순서로 학생 출력하기.py
@@ -1,18 +1,20 @@
 N = int(input())
 
-array = []
-for i in range(N):
-    input_data = input().split()
-    array.append((input_data[0], int(input_data[1])))
+#array = []
+#for i in range(N):
+#    input_data = input().split()
+#    array.append((input_data[0], int(input_data[1])))
 
-array = sorted(array, key=lambda student: student[1])
+#array = sorted(array, key=lambda student: student[1])
 
-#array = [input().split() for _ in range(N)]
-#for i in array:
-#    i[1] = int(i[1])
-#def setting(data):
-#    return data[1]
-#array = sorted(info, key=setting)
+array = [input().split() for _ in range(N)]
+for i in array:
+    i[1] = int(i[1])
+
+def setting(data):
+    return data[1]
+
+array = sorted(array, key=setting)
 
 for i in array:
     print(i[0], end=' ')
