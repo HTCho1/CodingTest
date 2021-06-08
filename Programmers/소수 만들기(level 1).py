d765bcc952b52ed9ba47e8b097bb3c2e9af86193
HTCho1 <sksmsghdxo@naver.com>
2021-06-09 01:34:22 +0900

    programmers level 1

--- Programmers/소수 만들기(level 1).py
+++ Programmers/소수 만들기(level 1).py
@@ -9,13 +9,34 @@ nums = list(map(int, sys.stdin.readline().split()))
 def solution(nums):
     nums.sort()
     comb = list(combinations(nums, 3))
-    print(comb)
-    answer = []
+    lst = []
     for i in comb:
-        print(i)
         result = sum(i)
-        for j in range(2, result):
-            if result % j != 0: answer.append(result)
-    return len(answer)
+        lst.append(isPrime(result))
+    return lst.count(True)
+
+def isPrime(N):
+    i = 2
+    while i * i <= N:
+        if N % i == 0: return False
+        i += 1
+    return True
+
+print(solution(nums))
+
+#-----------------------------------------------------------#
+
+nums = list(map(int, sys.stdin.readline().split()))
+
+def solution(nums):
+    answer = 0
+    comb = list(combinations(nums, 3))
+    for i in comb:
+        sum_num = sum(i)
+        for j in range(2, sum_num):
+            if sum_num % j == 0: break
+        else:
+            answer += 1
+    return answer
 
 print(solution(nums))
