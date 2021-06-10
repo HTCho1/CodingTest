b692d7c21af0ea5f461384f786b7b4f467116cc1
HTCho1 <sksmsghdxo@naver.com>
2021-06-10 17:11:34 +0900

    피보나치(Memoization)

--- 8. 다이나믹 프로그래밍/피보나치 수열.py
+++ 8. 다이나믹 프로그래밍/피보나치 수열.py
@@ -4,4 +4,6 @@ def fibo(num):
         return 1
     return fibo(num - 1) + fibo(num - 2)
 
-print(fibo(7))
+print(fibo(100))
+# 이 방식은 num이 커지면 커질수록 수행 시간이 기하급수적으로 늘어난다.
+# O(2**N) N ==30이면 10억 가량의 연산을 수행해야 한다.
