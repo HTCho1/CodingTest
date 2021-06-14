1a37e0be93d9e8fab411c1a29cee5cff66040f26
HTCho1 <sksmsghdxo@naver.com>
2021-06-14 23:54:30 +0900

    개미 전사

---
+++ 8. 다이나믹 프로그래밍/개미 전사.py
@@ -0,0 +1,18 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+storage = list(map(int, sys.stdin.readline().rstrip().split()))
+
+# 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+d = [0] * 100
+
+# 다이나믹 프로그래밍 (바텀업)
+d[0] = storage[0]
+d[1] = max(storage[0], storage[1])
+
+for i in range(2, N):
+    d[i] = max(d[i - 1], d[i - 2] + storage[i])
+
+#print(d)
+print(d[N - 1])
