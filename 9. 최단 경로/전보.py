9b795b27dc9fb74d980068989bd78ae3fb809571
HTCho1 <sksmsghdxo@naver.com>
2021-07-02 01:09:43 +0900

    전보

--- 9. 최단 경로/전보.py
+++ 9. 최단 경로/전보.py
@@ -30,16 +30,60 @@ def dijkstra(start):
 
 dijkstra(start)
 # C에서 다른 도시로 가는데 걸리는 시간을 저장할 리스트
+max_distance = 0
 d = []
 # C에서 보낸 메세지를 받는 도시의 개수
-t = 0
-for i in range(1, n + 1):
-    if (distance[i] == INF) or (distance[i] == 0):
-        continue
-    d.append(distance[i])
-    t += 1
+count = 0
+for d_ in distance:
+    if d_ != INF:
+        count += 1
+        d.append(d_)
 
-if len(d) == 0:
-    print(-1)
-else:
-    print(t, max(d))
+print(count - 1, max(d))
+
+########################################################################
+
+INF = int(1e9)
+
+n, m, start = map(int, sys.stdin.readline().rstrip().split())
+
+graph = [[] for _ in range(n + 1)]
+
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    x, y, z = map(int, sys.stdin.readline().rstrip().split())
+    graph[x].append((y, z))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+dijkstra(start)
+
+# 도달할 수 있는 노드의 개수
+count = 0
+# 도달할 수 있는 노드 중에서, 가장 멀리 있는 노드와의 최단 거리
+max_distance = 0
+for d in distance:
+    if d != INF:
+        count += 1
+        max_distance = max(max_distance, d)
+
+# 시작 노드는 제외해야 하므로 count - 1
+print(count - 1, max_distance)
